8d6bf62a29b48ec450cc833d4e6d6c50bb8818a4
Piyush Sharma <piyush19sha@gmail.com>
2018-04-10 14:25:44 -0400

    Update

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ particlesJS("particles-js",{
       }
     },
     "line_linked": {
-      "enable": true,
+      "enable": false,
       "distance": 150,
       "color": "#ffffff",
       "opacity": 0.4,
